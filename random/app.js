c136615e55b1c12967910f4f8028b7fcbc06b785
ArnavDekate <arnavdekate13@gmail.com>
2024-08-21 01:12:50 +0530

    Update app.js

--- random/app.js
+++ random/app.js
@@ -1,3 +1,21 @@
-console.log(`one`);
-console.log(`two`);
-console.log(`three`);
+
+// setTimeout(()=>{
+//     console.log(`hello`);
+// },5000);
+
+// function sum(a,b){
+//     console.log(a+b);
+// }
+
+// function mul(a,b){
+//     console.log(a*b);
+// }
+
+// function calc(a,b,callBack){
+//     callBack(a,b);
+//   }
+
+// calc(3,4,sum);
+// calc(3,4,mul);
+
+
